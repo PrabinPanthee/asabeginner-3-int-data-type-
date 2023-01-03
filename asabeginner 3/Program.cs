d801fd02083d22695f5736218e71beb15e44e77f
iampr <iampr@PRABIN>
2023-01-03 13:13:11 +0545

    Add project files.

---
+++ asabeginner 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main()
+    {
+     int i = 0;
+        Console.WriteLine("Max = {0}",int.MaxValue);
+        Console.WriteLine("Min = {0}", int.MinValue);
+
+
+
+
+            }
+}
+
